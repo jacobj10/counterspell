3b29134cc2873d7c9a41848993892f50a27d7370
Joshua Jacob <joshualaurencio@gmail.com>
2020-08-11 22:58:30 -0700

    Move to site dir

--- compile.sh
+++ compile.sh
@@ -1,2 +1,3 @@
-npx tsc --outFile static/counterspell.js counterspell.ts --lib es2015,dom
-cp node_modules/normalize.css/normalize.css static/normalize.css
+npx tsc --outFile site/static/counterspell.js counterspell.ts --lib es2015,dom
+cp node_modules/normalize.css/normalize.css site/static/normalize.css
+cp counterspell.html site/index.html
