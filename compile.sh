7a2d80759c1b26374fb623eb6b54808c9216d719
Joshua Jacob <joshualaurencio@gmail.com>
2020-08-11 23:03:00 -0700

    Add docs/ workaround

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 npx tsc --outFile site/static/counterspell.js counterspell.ts --lib es2015,dom
-cp node_modules/normalize.css/normalize.css site/static/normalize.css
-cp counterspell.html site/index.html
+cp node_modules/normalize.css/normalize.css docs/static/normalize.css
+cp counterspell.html docs/index.html
+cp counterspell.css docs/static/counterspell.css
